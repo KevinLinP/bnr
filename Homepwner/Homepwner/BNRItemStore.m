833be4cc8bd185afff751a1d99e77606e83a7356
Kevin Lin <kevin.lin.p@gmail.com>
2014-04-19 03:45:33 -0700

    Chapter 9: Gold attempt.

--- Homepwner/Homepwner/BNRItemStore.m
+++ Homepwner/Homepwner/BNRItemStore.m
@@ -56,4 +56,21 @@
     return item;
 }
 
+- (void)removeItem:(BNRItem *)item
+{
+    [self.privateItems removeObjectIdenticalTo:item];
+}
+
+- (void)moveItemAtIndex:(NSUInteger)fromIndex
+                toIndex:(NSUInteger)toIndex
+{
+    if (fromIndex == toIndex) {
+        return;
+    }
+    
+    BNRItem *item = self.privateItems[fromIndex];
+    [self.privateItems removeObjectIdenticalTo:item];
+    [self.privateItems insertObject:item atIndex:toIndex];
+}
+
 @end
--- Homepwner/Homepwner/BNRItemsViewController.m
+++ Homepwner/Homepwner/BNRItemsViewController.m
@@ -10,7 +10,10 @@
 #import "BNRItemStore.h"
 #import "BNRItem.h"
 
-@interface BNRItemsViewController () <UITableViewDataSource>
+@interface BNRItemsViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UIView *headerView;
+
 @end
 
 @implementation BNRItemsViewController
@@ -19,9 +22,6 @@
 {
     self = [super initWithStyle:UITableViewStylePlain];
     if (self) {
-        for (int i = 0; i < 5; i++) {
-            [[BNRItemStore sharedStore] createItem];
-        }
     }
     return self;
 }
@@ -31,21 +31,39 @@
     return [self init];
 }
 
+- (UIView *)headerView
+{
+    if (!_headerView) {
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView"
+                                      owner:self
+                                    options:nil];
+    }
+    return _headerView;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class]
            forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
-    NSArray *items = [[BNRItemStore sharedStore] allItems];
-    BNRItem *item = items[indexPath.row];
     
-    cell.textLabel.text = item.description;
+    if ([self isMessageIndexPath:indexPath]) {
+        cell.textLabel.text = @"No more items!";
+    } else {
+        NSArray *items = [[BNRItemStore sharedStore] allItems];
+        BNRItem *item = items[indexPath.row];
+        
+        cell.textLabel.text = item.description;
+    }
     
     return cell;
 }
@@ -53,7 +71,83 @@
 - (NSInteger)tableView:(UITableView *)tableView
  numberOfRowsInSection:(NSInteger)section
 {
-    return [[[BNRItemStore sharedStore] allItems] count];
+    NSInteger dataRows = [[[BNRItemStore sharedStore] allItems] count];
+
+    if (self.editing) { //TODO: breaks on swipe delete
+        return dataRows;
+    } else {
+        return dataRows + 1;
+    }
+    
+}
+
+- (IBAction)addNewItem:(id)sender
+{
+    BNRItem *newItem = [[BNRItemStore sharedStore] createItem];
+    NSInteger lastRow = [[[BNRItemStore sharedStore] allItems] indexOfObject:newItem];
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+- (IBAction)toggleEditingMode:(id)sender
+{
+    if (self.isEditing) {
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        [self setEditing:NO animated:YES];
+        
+        [self.tableView insertRowsAtIndexPaths:@[[self messageIndexPath]]
+                              withRowAnimation:UITableViewRowAnimationFade];
+    } else {
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        [self setEditing:YES animated:YES];
+        
+        [self.tableView deleteRowsAtIndexPaths:@[[self messageIndexPath]] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray *items = [[BNRItemStore sharedStore] allItems];
+        BNRItem *item = items[indexPath.row];
+        
+        [[BNRItemStore sharedStore] removeItem:item];
+        [tableView deleteRowsAtIndexPaths:@[indexPath]
+                         withRowAnimation:UITableViewRowAnimationRight];
+    }
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[BNRItemStore sharedStore] moveItemAtIndex:sourceIndexPath.row toIndex:destinationIndexPath.row];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"Remove";
+}
+
+- (NSIndexPath *)messageIndexPath
+{
+    NSInteger dataRows = [[[BNRItemStore sharedStore] allItems] count];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:dataRows inSection:0];
+    return indexPath;
+}
+
+- (BOOL)isMessageIndexPath:(NSIndexPath *)indexPath
+{
+    return indexPath.row == [self messageIndexPath].row;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return ![self isMessageIndexPath:indexPath];
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return ![self isMessageIndexPath:indexPath];
 }
 
 @end
