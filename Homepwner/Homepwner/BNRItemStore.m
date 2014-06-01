39442e2105c19a6f6998ca8720c735c5c4566746
Kevin Lin <kevin.lin.p@gmail.com>
2014-06-01 03:11:31 -0700

    Chapter 18: Added persistence of items and item store.

--- Homepwner/Homepwner/BNRItemStore.m
+++ Homepwner/Homepwner/BNRItemStore.m
@@ -41,7 +41,16 @@
 - (instancetype)initPrivate
 {
     self = [super init];
-    self.privateItems = [[NSMutableArray alloc] init];
+    
+    if (self) {
+        NSString *path = [self itemArchivePath];
+        _privateItems = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
+        
+        if (!_privateItems) {
+            _privateItems = [[NSMutableArray alloc] init];
+        }
+    }
+    
     return self;
 }
 
@@ -52,7 +61,7 @@
 
 - (BNRItem *)createItem
 {
-    BNRItem *item = [BNRItem randomItem];
+    BNRItem *item = [[BNRItem alloc] init];
     [self.privateItems addObject:item];
     return item;
 }
@@ -73,6 +82,21 @@
     BNRItem *item = self.privateItems[fromIndex];
     [self.privateItems removeObjectIdenticalTo:item];
     [self.privateItems insertObject:item atIndex:toIndex];
+}
+
+- (BOOL)saveChanges
+{
+    NSString *path = [self itemArchivePath];
+    
+    return [NSKeyedArchiver archiveRootObject:self.privateItems toFile:path];
+}
+
+- (NSString *)itemArchivePath
+{
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectory = [documentDirectories firstObject];
+    
+    return [documentDirectory stringByAppendingPathComponent:@"items.archive"];
 }
 
 @end
